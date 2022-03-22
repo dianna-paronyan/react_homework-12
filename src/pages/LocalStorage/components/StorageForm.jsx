e537daaa8409e18bd07531af45df2e0679ca99bf
dianna1234 <01234dianna@gmail.com>
2022-03-22 18:26:43 +0400

    changed

--- src/pages/LocalStorage/components/StorageForm.jsx
+++ src/pages/LocalStorage/components/StorageForm.jsx
@@ -6,21 +6,27 @@ function StorageForm(){
     const [emailInput, setEmailInput] = useState('');
     const [pwordlInput, setPwordInput] = useState('');
 
-    const {setItem1} = useLocalStorage('E-mail', '');
+    const {setItem1, clear} = useLocalStorage('E-mail', '');
 
     const {setItem2 } = useLocalStorage('Password', '');
 
     function handleEmailInput(e){
-        setEmailInput(e.target.value)
+        setEmailInput(e.target.value);
     }
+
     function handlePwordlInput(e){
-        setPwordInput(e.target.value)
+        setPwordInput(e.target.value);
     }
 
     function handleBtnClick(){
         setItem1(emailInput);
         setItem2(pwordlInput);
     }
+
+    function clearStorage(){
+        clear();
+    }
+
     return(
         <>
             <h1>Sign In</h1>
@@ -31,7 +37,7 @@ function StorageForm(){
                 <input type='password' placeholder='Password' name='password' className='pwordInput' onChange={handlePwordlInput} value={pwordlInput}/>
                 <button className='btn'  onClick={handleBtnClick}>Sign In</button>
             </form>
-
+            <button className='btn' onClick={clearStorage}>clear storage</button>
         </>
     )
 }
--- src/pages/LocalStorage/hooks/useLocalStorage.jsx
+++ src/pages/LocalStorage/hooks/useLocalStorage.jsx
@@ -2,27 +2,25 @@ import {useState} from 'react';
 
 
 function useLocalStorage(key, initValue){
-    const [data, setData] = useState(()=>{
-        getItem(key,initValue);
-    });
-    
+    const [data, setData] = useState();
     
     function getItem(key, initValue){
-
+           
         let savedValue = localStorage.getItem(key);
-        
         if(savedValue){
             return savedValue;
         }
-
+        
         return initValue;
     }
 
     const setItem1 = (value)=>{
+        setData(value);
         localStorage.setItem(key, JSON.stringify(value));
     }
 
     const setItem2 = (value)=>{
+        setData(value);
         localStorage.setItem(key, JSON.stringify(value));
     }
 
@@ -36,7 +34,7 @@ function useLocalStorage(key, initValue){
         localStorage.clear();
       }
 
-    return {data, setItem1, setItem2, removeItem, clear};
+    return {data, getItem, setItem1, setItem2, removeItem, clear};
 
 }
 
